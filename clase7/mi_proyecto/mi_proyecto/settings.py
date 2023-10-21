6bbcad61b2c7dc278c948c8fdb72eb98b6a3870d
mateotrucco <121066494+31M47RU@users.noreply.github.com>
2023-10-21 10:34:35 -0300

    agrego estilos

--- clase7/mi_proyecto/mi_proyecto/settings.py
+++ clase7/mi_proyecto/mi_proyecto/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mi_aplicacion/static')]
+STATICFILES_DIRS = ['mi_aplicacion/static']
+STATIC_ROOT = 'staticfiles'
 
 
 # Default primary key field type
